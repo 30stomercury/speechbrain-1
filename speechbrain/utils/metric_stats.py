7f43c778b82875b939bf82e7dd7648b2dd40b8b1
Sung-Lin Yeh <sunglin.yeh@ed.ac.uk>
2021-10-28 16:05:17 +0100

    Add metric for classification error

--- speechbrain/utils/metric_stats.py
+++ speechbrain/utils/metric_stats.py
@@ -6,6 +6,7 @@ Authors:
  * Mirco Ravanelli 2020
 """
 import torch
+import numpy as np
 from joblib import Parallel, delayed
 from speechbrain.utils.data_utils import undo_padding
 from speechbrain.utils.edit_distance import wer_summary, wer_details_for_batch
@@ -305,6 +306,100 @@ class ErrorRateStats(MetricStats):
 
         print_wer_summary(self.summary, filestream)
         print_alignments(self.scores, filestream)
+
+
+class ClsErrorRateStats(MetricStats):
+    """A class for tracking error rates (e.g., WER, PER).
+
+    Arguments
+    ---------
+    merge_tokens : bool
+        Whether to merge the successive tokens (used for e.g.,
+        creating words out of character tokens).
+        See ``speechbrain.dataio.dataio.merge_char``.
+    split_tokens : bool
+        Whether to split tokens (used for e.g. creating
+        characters out of word tokens).
+        See ``speechbrain.dataio.dataio.split_word``.
+    space_token : str
+        The character to use for boundaries. Used with ``merge_tokens``
+        this represents character to split on after merge.
+        Used with ``split_tokens`` the sequence is joined with
+        this token in between, and then the whole sequence is split.
+
+    Example
+    -------
+    >>> cer_stats = ErrorRateStats()
+    >>> i2l = {0: 'a', 1: 'b'}
+    >>> cer_stats.append(
+    ...     ids=['utterance1'],
+    ...     predict=torch.tensor([[0, 1, 1]]),
+    ...     target=torch.tensor([[0, 1, 0]]),
+    ...     target_len=torch.ones(1),
+    ...     ind2lab=lambda batch: [[i2l[int(x)] for x in seq] for seq in batch],
+    ... )
+    >>> stats = cer_stats.summarize()
+    >>> stats['WER']
+    33.33...
+    >>> stats['insertions']
+    0
+    >>> stats['deletions']
+    0
+    >>> stats['substitutions']
+    1
+    """
+
+    def __init__(self, merge_tokens=False, split_tokens=False, space_token="_"):
+        self.clear()
+        self.targets = []
+        self.predicts = []
+
+    def append(
+        self, ids, predict, target,
+    ):
+        """Add stats to the relevant containers.
+
+        * See MetricStats.append()
+
+        Arguments
+        ---------
+        ids : list
+            List of ids corresponding to utterances.
+        predict : torch.tensor
+            A predicted output, for comparison with the target output
+        target : torch.tensor
+            The correct reference output, for comparison with the prediction.
+        predict_len : torch.tensor
+            The predictions relative lengths, used to undo padding if
+            there is padding present in the predictions.
+        """
+        self.ids.extend(ids)
+        self.targets.extend(target)
+        self.predicts.extend(predict)
+
+    def summarize(self, field=None):
+        """Summarize the error_rate and return relevant statistics.
+
+        * See MetricStats.summarize()
+        """
+        self.summary = {}
+
+        # Add additional, more generic key
+        self.summary["error_rate"] = 1 - np.mean(
+            np.array(self.targets) == np.array(self.predicts)
+        )
+
+        if field is not None:
+            return self.summary[field]
+        else:
+            return self.summary
+
+    def write_stats(self, filestream):
+        """Write all relevant info (e.g., error rate alignments) to file.
+        * See MetricStats.write_stats()
+        """
+        if not self.summary:
+            self.summarize()
 
 
 class BinaryMetricStats(MetricStats):
